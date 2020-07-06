09319d142859065efb2c69e06c6f9d80f2e4841d
Noah Krohngold <Noah.Krohngold@gmail.com>
2020-07-06 19:01:44 +0000

    Add markers with custom logos to styled map

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -121,8 +121,306 @@ function deleteAllComments() {
 }
 
 function initMap() {
-    const map = new google.maps.Map(
-      document.getElementById('map'),
-      {center: {lat: 37.422, lng: -122.084}, zoom: 16}
+    var styledMapType = new google.maps.StyledMapType(
+        [
+            {
+                "elementType": "geometry",
+                "stylers": [
+                {
+                    "color": "#ebe3cd"
+                }
+                ]
+            },
+            {
+                "elementType": "labels.text.fill",
+                "stylers": [
+                {
+                    "color": "#523735"
+                }
+                ]
+            },
+            {
+                "elementType": "labels.text.stroke",
+                "stylers": [
+                {
+                    "color": "#f5f1e6"
+                }
+                ]
+            },
+            {
+                "featureType": "administrative",
+                "elementType": "geometry.stroke",
+                "stylers": [
+                {
+                    "color": "#c9b2a6"
+                }
+                ]
+            },
+            {
+                "featureType": "administrative.land_parcel",
+                "stylers": [
+                {
+                    "visibility": "off"
+                }
+                ]
+            },
+            {
+                "featureType": "administrative.land_parcel",
+                "elementType": "geometry.stroke",
+                "stylers": [
+                {
+                    "color": "#dcd2be"
+                }
+                ]
+            },
+            {
+                "featureType": "administrative.land_parcel",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                {
+                    "color": "#ae9e90"
+                }
+                ]
+            },
+            {
+                "featureType": "administrative.neighborhood",
+                "stylers": [
+                {
+                    "visibility": "off"
+                }
+                ]
+            },
+            {
+                "featureType": "landscape.natural",
+                "elementType": "geometry",
+                "stylers": [
+                {
+                    "color": "#dfd2ae"
+                }
+                ]
+            },
+            {
+                "featureType": "poi",
+                "elementType": "geometry",
+                "stylers": [
+                {
+                    "color": "#dfd2ae"
+                }
+                ]
+            },
+            {
+                "featureType": "poi",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                {
+                    "color": "#93817c"
+                }
+                ]
+            },
+            {
+                "featureType": "poi.park",
+                "elementType": "geometry.fill",
+                "stylers": [
+                {
+                    "color": "#a5b076"
+                }
+                ]
+            },
+            {
+                "featureType": "poi.park",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                {
+                    "color": "#447530"
+                }
+                ]
+            },
+            {
+                "featureType": "road",
+                "elementType": "geometry",
+                "stylers": [
+                {
+                    "color": "#f5f1e6"
+                }
+                ]
+            },
+            {
+                "featureType": "road",
+                "elementType": "labels",
+                "stylers": [
+                {
+                    "visibility": "off"
+                }
+                ]
+            },
+            {
+                "featureType": "road.arterial",
+                "elementType": "geometry",
+                "stylers": [
+                {
+                    "color": "#fdfcf8"
+                }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "geometry",
+                "stylers": [
+                {
+                    "color": "#f8c967"
+                }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "geometry.stroke",
+                "stylers": [
+                {
+                    "color": "#e9bc62"
+                }
+                ]
+            },
+            {
+                "featureType": "road.highway.controlled_access",
+                "elementType": "geometry",
+                "stylers": [
+                {
+                    "color": "#e98d58"
+                }
+                ]
+            },
+            {
+                "featureType": "road.highway.controlled_access",
+                "elementType": "geometry.stroke",
+                "stylers": [
+                {
+                    "color": "#db8555"
+                }
+                ]
+            },
+            {
+                "featureType": "road.local",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                {
+                    "color": "#806b63"
+                }
+                ]
+            },
+            {
+                "featureType": "transit.line",
+                "elementType": "geometry",
+                "stylers": [
+                {
+                    "color": "#dfd2ae"
+                }
+                ]
+            },
+            {
+                "featureType": "transit.line",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                {
+                    "color": "#8f7d77"
+                }
+                ]
+            },
+            {
+                "featureType": "transit.line",
+                "elementType": "labels.text.stroke",
+                "stylers": [
+                {
+                    "color": "#ebe3cd"
+                }
+                ]
+            },
+            {
+                "featureType": "transit.station",
+                "elementType": "geometry",
+                "stylers": [
+                {
+                    "color": "#dfd2ae"
+                }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "geometry.fill",
+                "stylers": [
+                {
+                    "color": "#b9d3c2"
+                }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "labels.text",
+                "stylers": [
+                {
+                    "visibility": "off"
+                }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                {
+                    "color": "#92998d"
+                }
+                ]
+            }
+        ],
+        {name: 'Styled Map'}
     );
+    var lindbergh = {lat: 47.454364, lng: -122.167713};
+    var landing = {lat: 47.498154, lng: -122.202927};
+    var cse2 = {lat: 47.653063, lng: -122.305106};
+    const map = new google.maps.Map(
+        document.getElementById('map'),
+        {
+            center: lindbergh,
+            zoom: 10,
+            draggable: true,
+            rotateControl: false,
+            tilt: false,
+            streetView: false,
+            streetViewControl: false,
+            streetViewControlOptions: false,
+            mapTypeControlOptions: {
+                mapTypeIds: ['styled_map']
+            }
+        }
+    );
+    var lhsImage = {
+        url: 'https://www.wpastatic.com/lib/ePoster/schools/155/school_logo_08272019220901.png',
+        scaledSize: new google.maps.Size(50,50),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(0, 0)
+    };
+    var washingtonLogo = {
+        url: 'https://upload.wikimedia.org/wikipedia/commons/3/36/University_of_Washington_Block_W_logo_RGB_brand_colors.SVG',
+        scaledSize: new google.maps.Size(50,50),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(0, 0)
+    };
+    var lindberghMarker = new google.maps.Marker(
+        {
+            position: lindbergh, 
+            map: map,
+            icon: lhsImage
+        });
+    var landingMarker = new google.maps.Marker(
+        {
+            position: landing, 
+            map: map
+        });
+    var cse2Marker = new google.maps.Marker(
+        {
+            position: cse2, 
+            map: map,
+            icon: washingtonLogo
+        });
+    map.mapTypes.set('styled_map', styledMapType);
+    map.setMapTypeId('styled_map');
 }
