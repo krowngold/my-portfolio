3de094610b8763cebd66f41b0afcf1dd8b86cd58
Noah Krohngold <Noah.Krohngold@gmail.com>
2020-07-07 18:27:20 +0000

    Add second chart that takes data from CSV and style the chart

--- portfolio/src/main/webapp/chart.js
+++ portfolio/src/main/webapp/chart.js
@@ -27,3 +27,32 @@ function drawUWChart() {
         document.getElementById("chart"));
     chart.draw(data, options);
 }
+
+google.charts.load('current', {'packages':['scatter']});
+google.charts.setOnLoadCallback(drawPlayChart);
+
+/** Creates chart of Google Play Store reviews and adds it to page */
+function drawPlayChart() {
+    fetch("/playstore-data").then(response => response.json())
+    .then((playStoreReviews) => {
+        const data = new google.visualization.DataTable();
+        data.addColumn("number", "Rating");
+        data.addColumn("number", "Installs");
+
+        for (let i = 0; i < playStoreReviews.length; i++) {
+            data.addRow([playStoreReviews[i][0], playStoreReviews[i][1]]);
+        }
+
+        const options = {
+            "title": "Play Store Reviews By Installs",
+            "width": 800,
+            "height": 500,
+            hAxis: {title: "Average Rating"},
+            vAxis: {title: "Number of Installs"}
+        };
+
+        const chart = new google.charts.Scatter(document.getElementById("chart2"));
+
+        chart.draw(data, google.charts.Scatter.convertOptions(options));
+    });
+}
